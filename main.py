8ae585e638ba60692cf11ec2e9000069a8d02684
beyzaaltuner <beyzaaltuner42@gmail.com>
2023-12-16 15:22:18 +0300

    uniform cost search

--- main.py
+++ main.py
@@ -8,7 +8,12 @@ class Node:
 
     def __lt__(self, other):
         return self.cost < other.cost
-    
+
+def manhattan_distance(point1, point2):
+    # Calculate Manhattan distance
+    x1, y1 = point1
+    x2, y2 = point2
+    return abs(x1 - x2) + abs(y1 - y2)    
 
 def uniform_cost_search(graph, start, goal):
     
